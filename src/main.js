a21081d07924e498b33ff625775adc298b681fa3
FishLiuWei <502152178@qq.com>
2019-11-22 23:51:23 +0800

    npm install axios --save npm install qs --save

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import App from './App.vue'
 import router from './router'
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
+import axios from 'axios';
+import QS from 'qs'
 
-Vue.use(MintUI)
+Vue.use(MintUI);
+Vue.prototype.$axios = axios;
+Vue.prototype.qs = QS;
 
 new Vue({
   el: '#app',
